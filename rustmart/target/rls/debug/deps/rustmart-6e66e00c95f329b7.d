2d1284f75be27242d46b9ed2095faac2b47a8f24
Erik WSL <go.erik@protonmail.com>
2020-08-26 14:47:47 -0700

    migrate hello world to an initial component

--- rustmart/target/rls/debug/deps/rustmart-6e66e00c95f329b7.d
+++ rustmart/target/rls/debug/deps/rustmart-6e66e00c95f329b7.d
@@ -1,5 +1,7 @@
-/mnt/c/Users/goodm/Code/learning/rust-wasm-spa/rustmart/target/rls/debug/deps/rustmart-6e66e00c95f329b7.rmeta: src/lib.rs
+/mnt/c/Users/goodm/Code/learning/rust-wasm-spa/rustmart/target/rls/debug/deps/rustmart-6e66e00c95f329b7.rmeta: src/lib.rs src/pages/mod.rs src/pages/home.rs
 
-/mnt/c/Users/goodm/Code/learning/rust-wasm-spa/rustmart/target/rls/debug/deps/rustmart-6e66e00c95f329b7.d: src/lib.rs
+/mnt/c/Users/goodm/Code/learning/rust-wasm-spa/rustmart/target/rls/debug/deps/rustmart-6e66e00c95f329b7.d: src/lib.rs src/pages/mod.rs src/pages/home.rs
 
 src/lib.rs:
+src/pages/mod.rs:
+src/pages/home.rs:
--- rustmart/target/wasm32-unknown-unknown/debug/deps/rustmart.d
+++ rustmart/target/wasm32-unknown-unknown/debug/deps/rustmart.d
@@ -1,7 +1,9 @@
-/mnt/c/Users/goodm/Code/learning/rust-wasm-spa/rustmart/target/wasm32-unknown-unknown/debug/deps/rustmart.wasm: src/lib.rs
+/mnt/c/Users/goodm/Code/learning/rust-wasm-spa/rustmart/target/wasm32-unknown-unknown/debug/deps/rustmart.wasm: src/lib.rs src/pages/mod.rs src/pages/home.rs
 
-/mnt/c/Users/goodm/Code/learning/rust-wasm-spa/rustmart/target/wasm32-unknown-unknown/debug/deps/librustmart.rlib: src/lib.rs
+/mnt/c/Users/goodm/Code/learning/rust-wasm-spa/rustmart/target/wasm32-unknown-unknown/debug/deps/librustmart.rlib: src/lib.rs src/pages/mod.rs src/pages/home.rs
 
-/mnt/c/Users/goodm/Code/learning/rust-wasm-spa/rustmart/target/wasm32-unknown-unknown/debug/deps/rustmart.d: src/lib.rs
+/mnt/c/Users/goodm/Code/learning/rust-wasm-spa/rustmart/target/wasm32-unknown-unknown/debug/deps/rustmart.d: src/lib.rs src/pages/mod.rs src/pages/home.rs
 
 src/lib.rs:
+src/pages/mod.rs:
+src/pages/home.rs:
